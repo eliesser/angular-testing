98794471da31f2c2422e288685915324a9364980
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-11 15:09:26 +0200

    add test validators

--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -1,12 +1,13 @@
 import { AbstractControl } from '@angular/forms';
+import { UsersService } from '../services/user/user.service';
+import { map } from 'rxjs';
 
 export class MyValidators {
-
   static isPriceValid(control: AbstractControl) {
     const value = control.value;
-    console.log(value);
+
     if (value > 10000) {
-      return {price_invalid: true};
+      return { price_invalid: true };
     }
     return null;
   }
@@ -14,18 +15,21 @@ export class MyValidators {
   static validPassword(control: AbstractControl) {
     const value = control.value;
     if (!containsNumber(value)) {
-      return {invalid_password: true};
+      return { invalid_password: true };
     }
-    return null;
+    return null; // null === todo bien
   }
 
   static matchPasswords(control: AbstractControl) {
     const password = control?.get('password')?.value;
     const confirmPassword = control?.get('confirmPassword')?.value;
-    if (password !== confirmPassword) {
-      return {match_password: true};
+    if (password === undefined || confirmPassword === undefined) {
+      throw new Error('matchPasswords: fields not found');
     }
-    return null;
+    if (password !== confirmPassword) {
+      return { match_password: true };
+    }
+    return null; // null === todo bien
   }
 
   // static validateCategory(service: CategoriesService) {
@@ -44,13 +48,26 @@ export class MyValidators {
   //   };
   // }
 
+  static validateEmailAsync(service: UsersService) {
+    return (control: AbstractControl) => {
+      const value = control.value;
+      return service.isAvailableByEmail(value).pipe(
+        map((response) => {
+          const isAvailable = response.isAvailable;
+          if (!isAvailable) {
+            return { not_available: true };
+          }
+          return null;
+        })
+      );
+    };
+  }
 }
 
-function containsNumber(value: string){
-  return value.split('').find(v => isNumber(v)) !== undefined;
+function containsNumber(value: string) {
+  return value.split('').find((v) => isNumber(v)) !== undefined;
 }
 
-
-function isNumber(value: string){
+function isNumber(value: string) {
   return !isNaN(parseInt(value, 10));
 }
